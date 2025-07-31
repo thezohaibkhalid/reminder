929215fc33b9d0fba835c728260efcf942208a71
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-07-31 23:15:16 +0500

    added tabs navigator for routing

---
+++ app/(tabs)/Settings.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+const SettingsScreen  = ()=>{
+    return(
+        <View>
+            <Text>
+                Hi i am zohaib khalid it is settings page
+            </Text>
+        </View>
+    )
+}
+export default SettingsScreen;
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,44 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import {Ionicons} from "@expo/vector-icons";
+import {Tabs} from "expo-router";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+export default function TabsLayout() {
+    return (
+        <Tabs screenOptions={{
+            tabBarActiveTintColor:"red",
+            tabBarInactiveTintColor:"green",
+            tabBarStyle:{
+                backgroundColor:"#f0f0ff",
+                borderTopWidth: "1",
+                borderTopColor:"black",
+                paddingTop:"3"
+            },
+            tabBarLabelStyle: {
+                fontSize:12,
+                fontWeight:"bold"
+            },
+            headerShown:false
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+        }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title:"Todos",
+                    tabBarIcon:({color, size})=>(
+                        <Ionicons name={'flash-outline'}  size={size} color={color}/>
+                    )
+                }}
+                />
+            <Tabs.Screen
+                name="Settings"
+                options={{
+                    title:"Settings",
+                    tabBarIcon:({color, size})=>(
+                        <Ionicons name={'settings'} size={size} color={color}/>
+                    )
+                }}
+            />
+        </Tabs>
+    );
 }
+
